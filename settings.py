8b1017b989b8063759482e3314907f10e83211e4
Flavio Luiz Mendes <flmendes@gmail.com>
2011-04-18 11:44:46 -0300

    correção da estrutura do template

--- settings.py
+++ settings.py
@@ -2,17 +2,7 @@ import os
 PROJECT_ROOT_PATH = os.path.dirname(__file__)
 
 TEMPLATE_DIRS = (
-os.path.join(PROJECT_ROOT_PATH, 'templates'),
+    os.path.join(PROJECT_ROOT_PATH, 'templates'),
 )
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-INTERNAL_IPS = ('127.0.0.1',)
-
-MEDIA_ROOT = os.path.join(PROJECT_ROOT_PATH, 'media')
-
-CURRENCIES = (
-('BRL', 'Real'),
-('USD', 'US Dollar'),
-)
+STATIC_URL = 'static/'
